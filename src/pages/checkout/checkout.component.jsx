d1923013bb1b9e01f1230a98e28b1d145cb353cf
valy-s <valeriu.stanculescu@gmail.com>
2021-05-18 11:21:27 -0500

    moved directory and shop data into its own reduce,r

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -12,30 +12,36 @@ import {
 import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
-  <div className="checkout-page">
-    <div className="checkout-header">
-      <div className="header-block">
-        <span>Product</span>
+  <div className="checkout-page-container">
+    {cartItems.length ? (
+      <div className="checkout-page">
+        <div className="checkout-header">
+          <div className="header-block">
+            <span>Product</span>
+          </div>
+          <div className="header-block">
+            <span>Description</span>
+          </div>
+          <div className="header-block">
+            <span>Quantity</span>
+          </div>
+          <div className="header-block">
+            <span>Price</span>
+          </div>
+          <div className="header-block">
+            <span>Remove</span>
+          </div>
+        </div>
+        {cartItems.map((cartItem) => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+        <div className="total">
+          <span>Total: ${total}</span>
+        </div>
       </div>
-      <div className="header-block">
-        <span>Description</span>
-      </div>
-      <div className="header-block">
-        <span>Quantity</span>
-      </div>
-      <div className="header-block">
-        <span>Price</span>
-      </div>
-      <div className="header-block">
-        <span>Remove</span>
-      </div>
-    </div>
-    {cartItems.map((cartItem) => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
-    <div className="total">
-      <span>Total: ${total}</span>
-    </div>
+    ) : (
+      <h1> No items in the cart</h1>
+    )}
   </div>
 );
 
